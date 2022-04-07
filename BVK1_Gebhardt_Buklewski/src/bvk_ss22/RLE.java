6bef427925c9ef95360b793d8fb838a49a01982b
KBuklewski <45571879+KBuklewski@users.noreply.github.com>
2022-04-07 17:00:41 +0200

    Update RLE.java

--- BVK1_Gebhardt_Buklewski/src/bvk_ss22/RLE.java
+++ BVK1_Gebhardt_Buklewski/src/bvk_ss22/RLE.java
@@ -6,6 +6,8 @@
 
 package bvk_ss22;
 
+import com.sun.org.apache.bcel.internal.generic.NEWARRAY;
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -21,11 +23,23 @@ public class RLE {
 	public static RasterImage decodeImage(DataInputStream in) throws IOException {
 		int width = 10;
 		int height = 10;
-		
+
+		int numberOfColors;
+		int argb;
+
+		int[] colors;
 		// TODO: read width and height from DataInputStream
 //		width = ...;
 //		height = ...;
-		
+		width = in.readInt();
+		height = in.readInt();
+		numberOfColors = in.readInt();
+
+		for(int i=0; i<=numberOfColors; i++){
+			
+		}
+
+		System.out.println(width + " " + height);
 		// create RasterImage to be returned
 		RasterImage image = new RasterImage(width, height);
 
