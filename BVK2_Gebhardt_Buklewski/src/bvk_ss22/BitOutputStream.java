daf255e24a3a60b71fc3ffbcd188f0571ed6c68f
KBuklewski <45571879+KBuklewski@users.noreply.github.com>
2022-04-27 20:53:02 +0200

    Bit input und output class

---
+++ BVK2_Gebhardt_Buklewski/src/bvk_ss22/BitOutputStream.java
@@ -0,0 +1,18 @@
+package bvk_ss22;
+
+import java.io.OutputStream;
+
+public class BitOutputStream {
+
+    OutputStream out;
+
+    public BitOutputStream (OutputStream out){
+        this.out = out;
+    }
+    public void write(int value, int bitnumber){
+
+    }
+    public void close(){
+        
+    }
+}
