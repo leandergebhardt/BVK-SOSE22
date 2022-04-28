05af34e7a8d68dddf8dadd61cdc2b969d5827d4d
KBuklewski <45571879+KBuklewski@users.noreply.github.com>
2022-04-28 21:41:42 +0200

    Update BitOutputStream.java

--- BVK2_Gebhardt_Buklewski/src/bvk_ss22/BitOutputStream.java
+++ BVK2_Gebhardt_Buklewski/src/bvk_ss22/BitOutputStream.java
@@ -6,11 +6,14 @@ public class BitOutputStream {
 
     OutputStream out;
 
+    Integer[] buffer;
     public BitOutputStream (OutputStream out){
         this.out = out;
     }
     public void write(int value, int bitnumber){
-
+        for(int x = bitnumber; x == 0; x--) {
+            buffer[x] = (value >> x) & 1;
+        }
     }
     public void close(){
         
