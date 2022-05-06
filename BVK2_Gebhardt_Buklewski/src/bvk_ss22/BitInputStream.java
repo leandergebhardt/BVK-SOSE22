80806ad0a89e9a97cbbc7ae719175f20930b6e41
KBuklewski <45571879+KBuklewski@users.noreply.github.com>
2022-05-06 11:45:40 +0200

    Update BitInputStream.java

--- BVK2_Gebhardt_Buklewski/src/bvk_ss22/BitInputStream.java
+++ BVK2_Gebhardt_Buklewski/src/bvk_ss22/BitInputStream.java
@@ -15,24 +15,60 @@ public class BitInputStream {
         }
 
         int count = 0;
+        int bufferCount = 8;
+        int rcount = 0;
+        int r = 0;
+        int i;
         public int read(int bitNumber) throws IOException {
-                count = 0;
-                int i = in.read();
 
-                for (int x = 8; x>0 ; x--) {
-                        int b = (i >> x-1) & 1;
-                        buffer[count] = b;
-                        count++;
+                if(bufferCount==0) {
+                        i = in.read();
 
 
+                        for (int x = 8; x > 0; x--) {           //neuen Buffer einlesen
+                                int b = (i >> x - 1) & 1;
+                                buffer[count] = b;
+                                count++;
+                                bufferCount--;
+                        }
+                        r=0;
+                        rcount=0;
+                        for (int x = bitNumber-1; rcount != bitNumber; x--) {
+                                r = r | (buffer[rcount] << x);
+                                rcount++;
+                                bufferCount++;
+                        }
                 }
-                count = 0;
-                int r = 0;
-                for (int x = bitNumber-1; count != bitNumber; x--) {
-                        r = r | (buffer[count] << x);
-                        count++;
+                else{                                   //es sind noch zahlen im buffer
+                        r=0;
+                        rcount=0;
+                        while(bufferCount<8) {
+                                for (int x = bitNumber - 1; bufferCount<8; x--) {
+                                        r = r | (buffer[bufferCount] << x);
+                                        rcount++;
+                                        bufferCount++;
+                                }
 
+                        }
+                        if(rcount!=bitNumber) {
+                                i = in.read();
+                                count = 0;
+                                for (int x = 8; x > 0; x--) {           //neuen Buffer einlesen
+                                        int b = (i >> x - 1) & 1;
+                                        buffer[count] = b;
+                                        count++;
+                                        bufferCount--;
+                                }
+                                for (int x = bitNumber - 1; rcount != bitNumber; x--) {
+                                        r = r | (buffer[bufferCount] << x);
+                                        rcount++;
+                                        bufferCount++;
+                                }
+                        }
                 }
+
+
+
                 return r;
         }
 
