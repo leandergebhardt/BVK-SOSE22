e1f0ca32fc68bd47de1a5e927880c140040e5240
Leander Gebhardt <Leander.Gebhardt@Student.HTW-Berlin.de>
2022-05-14 21:06:52 +0200

    BitOutpuStream working

--- BVK2_Gebhardt_Buklewski/src/bvk_ss22/BitOutputStream.java
+++ BVK2_Gebhardt_Buklewski/src/bvk_ss22/BitOutputStream.java
@@ -1,5 +1,6 @@
 package bvk_ss22;
 
+import java.io.IOException;
 import java.io.OutputStream;
 
 public class BitOutputStream {
@@ -13,22 +14,46 @@ public class BitOutputStream {
 
     }
 
+    int bufferCount = 0;
+    int count = 0;
 
-
-    public Integer write(int value, int bitnumber){
+    public void write(int value, int bitnumber) throws IOException {
 
         for(int x = bitnumber-1; x >= 0; x--) {
-             int b = (value >>x) & 1;
-            buffer[x] = b;                          //der part funktioniert
+            int bit = (value >> x) & 1;
+            buffer[count] = bit;
+            count++;
+            bufferCount++;
 
+            if (bufferCount == 8) {
+                int r = 0;
+                int rcount = 0;
+                for (int y = 8; y > 0; y--) {
+                    r = r | (buffer[rcount] << y-1);
+                    buffer[rcount] = null;
+                    rcount++;
+                }
+                bufferCount = 0;
+                count = 0;
+                out.write(r);
+            }
         }
-        int r = 0;
-        for(int x = bitnumber-1; x >= 0; x--){
-            r= r | (buffer[x]<<x);                                       //passt
-        }
-        return r;
+
+
     }
-    public void close(){
-        
+
+    public void close() throws IOException {
+
+        int r = 0;
+        int count = 0;
+        for(int x = 8; x >0; x--){
+            if(buffer[count] == null){buffer[count] = 0;}
+            r= r | (buffer[count]<<x-1);
+            count++;
+        }
+
+
+        out.write(r);
+        out.close();
     }
 }
