0eb8908269039e89e9f71eea46be1c92503be51b
KBuklewski <45571879+KBuklewski@users.noreply.github.com>
2022-06-15 21:18:06 +0200

    Update Wavelet.java

--- BVK5_Gebhardt_Buklewski/src/bvk_ss22/Wavelet.java
+++ BVK5_Gebhardt_Buklewski/src/bvk_ss22/Wavelet.java
@@ -50,43 +50,79 @@ public class Wavelet {
 		double[] h0 = new double[] {-1.0, 2.0, 6.0, 2.0, -1.0};
 		double[][] resultImage = new double[image.length][image[0].length];
 			// durch x&y iterieren und h0 als filter anwenden; Randbehandlung: spiegeln
-			for(int y = 0; y < image.length; y++){
-				for(int x = 0; x < image[0].length; x++){
-					System.out.println("pixelvalue before: " + image[y][x]);
+			for(int y = 0; y < image.length; y++) {
+
+				for (int x = 0; x < image[0].length; x++) {
+					if (x % 2 == 0){
+						System.out.println("pixelvalue before: " + image[y][x]);
 					//filter anwenden
 					int count = 0;
-					double value=0;
+					double value = 0;
+
 					for (int f = -2; f <= 2; f++) {
 						int i = f;
-						if(x+i < 0){i = i * (-1);}
-						if(x+i >= image[0].length){i = i * (-1);}
+						if (x + i < 0) {
+							i = i * (-1);
+						}
+						if (x + i >= image[0].length) {
+							i = i * (-1);
+						}
 
-						value += image[y][x+i] * h0[count];
+						value += image[y][x + i] * h0[count];
 
 						count++;
 					}
 
-					value = value/8;
-					
+					value = value / 8.0;
+
 					System.out.println(value);
 
-
-					resultImage[y][x] = value;
+					resultImage[y][x/2] = value;
 				}
+			}
 			}
 		return resultImage;
 	}
 
-	public double[][] highPassHorizontal (double[][] image){
+	public static double[][] highPassHorizontal(double[][] image){
 		double[] h1 = new double[] {-1.0, 2.0, -1.0};
-			//
+		double[][] resultImage = new double[image.length][image[0].length];
+		// durch x&y iterieren und h0 als filter anwenden; Randbehandlung: spiegeln
+		for(int y = 0; y < image.length; y++){
+			for(int x = 0; x < image[0].length; x++){
+				System.out.println("pixelvalue before: " + image[y][x]);
+				//filter anwenden
+				int count = 0;
+				double value=0;
 
-		return image;
+				for (int f = -1; f <= 1; f++) {
+					int i = f;
+					if(x+i < 0){i = i * (-1);}
+					if(x+i >= image[0].length){i = i * (-1);}
+
+					value += image[y][x+i] * h1[count];
+
+					count++;
+				}
+
+				value = value/2.0;
+
+				value += 127;
+
+				System.out.println(value);
+
+
+				resultImage[y][x] = value;
+			}
+		}
+		return resultImage;
+
 	}
 
 	public static RasterImage testConverter(RasterImage input){
 		double[][] test = convertImageTo2DArray(input);
 		test = lowPassHorizontal(test);
+		//test = highPassHorizontal(test);
 		return convert2DArrayToImage(test);
 	}
 
@@ -96,7 +132,8 @@ public class Wavelet {
 		for(int y = 0; y < input.height; y++){
 			for(int x = 0; x < input.width; x++){
 				int pos = y * input.width + x;
-				result[y][x] = input.argb[pos];
+				double grey = input.argb[pos] & 0xff;
+				result[y][x] = grey;
 			}
 		}
 
@@ -111,7 +148,9 @@ public class Wavelet {
 		for(int y = 0; y < input.length; y++){
 			for(int x = 0; x < input[0].length; x++){
 				int pos = y * input[0].length + x;
-				result.argb[pos] = (int) input[y][x];
+
+				int avg  = (int) input[y][x];
+				result.argb[pos] = (0xff << 24) | (avg << 16) | (avg << 8) | avg;
 			}
 		}
 
