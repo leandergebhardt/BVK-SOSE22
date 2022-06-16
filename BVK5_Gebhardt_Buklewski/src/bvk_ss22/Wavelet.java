150d5e731d7eecb229f900344900e09b336c890b
Leander Gebhardt <38793421+leandergebhardt@users.noreply.github.com>
2022-06-16 16:59:36 +0200

    1. kaskade wavelet

--- BVK5_Gebhardt_Buklewski/src/bvk_ss22/Wavelet.java
+++ BVK5_Gebhardt_Buklewski/src/bvk_ss22/Wavelet.java
@@ -11,34 +11,6 @@ public class Wavelet {
 	public static double[][] kaskade(double[][] input){
 		double[][] result = new double[input.length][input.length];
 
-		double[] h0 = new double[] {-1.0, 2.0, 6.0, 2.0, -1.0};
-		double[] h1 = new double[] {-1.0, 2.0, -1.0};
-
-		double[] g0 = new double[] {1.0, 2.0, 1.0};
-		double[] g1 = new double[] {-1.0, -2.0, 6.0, -2.0, -1.0};
-
-		int indexh0 = 0;		//wofür sind diese berechnungen?
-		for(double element : h0){
-			h0[indexh0] = element * 0.125;
-			indexh0++;
-		}
-
-		int indexh1 = 0;
-		for(double element : h1){
-			h1[indexh1] = element * 0.5;
-			indexh1++;
-		}
-		int indexg0 = 0;
-		for(double element : g0){
-			g0[indexg0] = element * 0.5;
-			indexg0++;
-		}
-
-		int indexg1 = 0;
-		for(double element : g1){
-			g1[indexg1] = element * 0.125;
-			indexg1++;
-		}
 
 
 		return result;
@@ -78,6 +50,7 @@ public class Wavelet {
 				}
 			}
 			}
+
 		return resultImage;
 	}
 
@@ -87,7 +60,7 @@ public class Wavelet {
 		// durch x&y iterieren und h0 als filter anwenden; Randbehandlung: spiegeln
 		for(int y = 0; y < image.length; y++) {
 			for (int x = 0; x < image[0].length; x++) {
-				if (x % 2 == 0){
+				if (y % 2 == 0){
 					System.out.println("pixelvalue before: " + image[y][x]);
 					//filter anwenden
 					int count = 0;
@@ -111,10 +84,11 @@ public class Wavelet {
 
 					System.out.println(value);
 
-					resultImage[y][x/2] = value;
+					resultImage[y/2][x] = value;
 				}
 			}
 		}
+
 		return resultImage;
 	}
 
@@ -124,7 +98,7 @@ public class Wavelet {
 		// durch x&y iterieren und h0 als filter anwenden; Randbehandlung: spiegeln
 		for(int y = 0; y < image.length; y++){
 			for(int x = 0; x < image[0].length; x++){
-				if (y % 2 == 0) {
+				if (x % 2 == 0) {
 					System.out.println("pixelvalue before: " + image[y][x]);
 					//filter anwenden
 					int count = 0;
@@ -151,10 +125,11 @@ public class Wavelet {
 					System.out.println(value);
 
 
-					resultImage[y/2][x] = value;
+					resultImage[y][x/2] = value;
 				}
 			}
 		}
+
 		return resultImage;
 
 	}
@@ -195,15 +170,66 @@ public class Wavelet {
 				}
 			}
 		}
+
 		return resultImage;
 
 	}
 
 	public static RasterImage testConverter(RasterImage input){
-		double[][] test = convertImageTo2DArray(input);
-		test = highPassHorizontal(test);
-		test = lowPassVertical(test);
-		return convert2DArrayToImage(test);
+		double[][] source = convertImageTo2DArray(input);
+
+		double[][] HL = highPassHorizontal(source);
+		HL = lowPassVertical(HL);
+
+		double[][] LH = lowPassHorizontal(source);
+		LH = highPassVertical(LH);
+
+		double[][] LL = lowPassHorizontal(source);
+		LL = lowPassVertical(LL);
+
+		double[][] HH = highPassHorizontal(source);
+		HH = highPassVertical(HH);
+
+		// Bilder zuschneiden
+		HL = fitArray(HL);
+		LH = fitArray(LH);
+		LL = fitArray(LL);
+		HH = fitArray(HH);
+		double[][] result = stitchTogether(LL, LH, HL, HH);
+
+		return convert2DArrayToImage(result);
+	}
+
+	private static double[][] stitchTogether(double[][] ll, double[][] lh, double[][] hl, double[][] hh) {
+		int newHeight = ll.length * 2;
+		int newWidth = ll[0].length * 2;
+		double[][] result = new double[newHeight][newWidth];
+
+		int offsetY = newHeight / 2;
+		int offsetX = newWidth / 2;
+
+		for(int y = 0; y < newHeight; y++){
+			for(int x = 0; x < newWidth; x++) {
+				// LL Pass oben links
+				if(x < newWidth / 2 && y < newHeight / 2 && x < newWidth && y < newHeight){
+					result[y][x] = ll[y][x];
+				}
+				// LH oben rechts
+				if(x > newWidth / 2 && y < newHeight / 2 && x < newWidth && y < newHeight){
+					result[y][x] = Math.round(lh[y][x -offsetX]*2 + 127);
+				}
+				// HL unten links
+				if(x < newWidth / 2 && y > newHeight / 2 && x < newWidth && y < newHeight){
+					result[y][x] = Math.round(hl[y - offsetY][x]*2 + 127);
+				}
+				// HH unten rechts
+				if(x > newWidth / 2 && y > newHeight / 2 && x < newWidth && y < newHeight){
+					result[y][x] = Math.round(hh[y - offsetY][x - offsetX]*2 + 127);
+				}
+			}
+		}
+
+		return result;
 	}
 
 	public static double[][] convertImageTo2DArray(RasterImage input){
@@ -220,16 +246,33 @@ public class Wavelet {
 		return result;
 	}
 
+	public static double[][] fitArray(double[][] input){
+		int newWidth = input[0].length / 2;
+		int newHeight = input.length / 2;
+
+		double[][] fittedArray = new double[newHeight][newWidth];
+
+		for(int y = 0; y < newHeight; y++){
+			for(int x = 0; x < newWidth; x++) {
+				fittedArray[y][x] = input[y][x];
+			}
+		}
+
+		return fittedArray;
+	}
+
 	public static RasterImage convert2DArrayToImage(double[][] input){
-		RasterImage result = new RasterImage(input[0].length, input.length);
+		int width = input[0].length;
+		int height = input.length;
+		RasterImage result = new RasterImage(width, height);
 
 		System.out.println("width: " + input[0].length + "px * height: " + input.length + "px");
 
-		for(int y = 0; y < input.length; y++){
-			for(int x = 0; x < input[0].length; x++){
-				int pos = y * input[0].length + x;
+		for(int y = 0; y < height; y++){
+			for(int x = 0; x < width; x++){
+				int pos = y * width + x;
 
-				int avg  = (int) Math.round(input[y][x]*2 + 127);
+				int avg  = (int) Math.round(input[y][x]);
 				if(avg < 0){ avg = 0;}
 				if(avg > 255){avg = 255;}
 				result.argb[pos] = (0xff << 24) | (avg << 16) | (avg << 8) | avg;
